c5e648dfaabfb12e441218de86299e874db6ffcb
Heisen-erg <heisenberg.database.1@gmail.com>
2023-09-16 22:36:59 +0530

    hh

--- app/page.js
+++ app/page.js
@@ -1,30 +1,37 @@
 "use client"
 import React from "react";
 import Bottombar from "@/Components/Bottombar";
-import Mainscreen from "@/Components/Mainscreen";
+
 import Topbar from "@/Components/Topbar";
 import { useEffect } from "react";
 import axios from "axios"
-import Middlebar from "@/Components/Middlebar";
+
 import Leftbar from "@/Components/Leftbar";
 import Card1 from "@/Components/Card1"
 import dhaage from "@/public/assets/dhaage.png"
 import CustomSpinner from "@/Components/CustomSpinner";
-import Tanishq from "@/Components/Tanishq"
+import InfiniteScroll from "react-infinite-scroll-component";
 import { Spinner } from "@nextui-org/react";
 export default function Home() {
 const[data,setdata]=React.useState([])
 const[spinner,setspinner]=React.useState(true)
-useEffect(() => {
-  axios.get("/thread/user").then((response)=>{setdata((response.data).reverse())
-    return ( setspinner(false))})
+// useEffect(() => {
+//   axios.get("/thread/user").then((response)=>{setdata((response.data).reverse())
+//     return ( setspinner(false))})
     
   
   
   
 
-}, )
+// }, )
+// const fetchMoreData = () => {
 
+//   setTimeout(() => {
+//    setState({
+//       data: this.state.items.concat(Array.from({ length: 3}))
+//     });
+//   }, 1500);
+// };
 
 
 
@@ -38,17 +45,32 @@ useEffect(() => {
 
 <div className='flex   ' >
         <Leftbar/>
-        {spinner? <CustomSpinner/> :
+        {/* {spinner? <CustomSpinner/> : */}
         <div style={{height:'fit-content',minHeight:'80vh'}}  className='flex  items-center flex-col gap-6 z-0 text-gray-400 w-full  lg:w-5/6 '>
           {/* <Card1 photo={dhaage} thread={"#THIS IS THREADS(CLONE)  #CURRENTLY UNDER DEVELOPMENT #BUT YOU CAN CREATE YOUR THREAD UNDER CREATE SECTION "} /> */}
-         
+          {/* <InfiniteScroll
+  dataLength={data.length} 
+  next={fetchMoreData}
+  hasMore={true}
+  loader={<h4>Loading...</h4>}
+  endMessage={
+    <p style={{ textAlign: 'center' }}>
+      <b>Yay! You have seen it all</b>
+    </p>
+  }>
 {data.map((response)=>{
 
-  return  <Card1 photo={response.image} postimage={response.postimage} username={response.using} thread={response.thread} key={response._id} id={response._id} />
-})}
+   return   <Card1 photo={response.image} postimage={response.postimage} username={response.using} thread={response.thread} key={response._id} id={response._id} />
+})} 
+</InfiniteScroll>    */}
 
 
-   </div>}
+    <div className='bg-black text-3xl text-white text-center h-screen w-full'>⚠️⚠️⚠️⚠️⚠️⚠️ UNDER MAINTAINANCE</div>
+ 
+
+export default page
+   </div>
+     
      
     </div>
 
@@ -65,3 +87,7 @@ useEffect(() => {
   
  )
 }
+
+
+
+
